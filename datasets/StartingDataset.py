Fixed issue with sizing and scaling. TODO: map the label of each image to an int

datasets/StartingDataset.py
@@ -7,20 +7,27 @@ class StartingDataset(torch.utils.data.Dataset):
     Dataset that contains 100000 3x224x224 black images (all zeros).
     """
 
-    def __init__(self, csv_path, image_dir):
+    def __init__(self, csv_path, image_dir, image_size):
         self.images_frame = pd.read_csv(csv_path)
         self.image_dir = image_dir
+        self.image_size = image_size
 
         #load in this one image at a time every epoch
     def __getitem__(self, index):
-        if torch.is_tensor(index):
-            index = index.tolist
+        """if torch.is_tensor(index):
+            index = index.tolist"""
         
         label = self.images_frame.iloc[index, 1]
         image_path = self.image_dir + self.images_frame.iloc[index, 0]
         image = torchvision.io.read_image(image_path)
 
-        return image, label
-
+        if(len(image)==3):
+            image = torchvision.transforms.Grayscale().forward(image)
+        image = torchvision.transforms.Resize(self.image_size).forward(image)
+        image = image.float()
+        
+        return image, label #map the label to an int
+    
     def __len__(self):
         return len(self.images_frame)
+

train_functions/starting_train.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.utils.tensorboard
-
+import numpy as np
 
 def starting_train(
     train_dataset, val_dataset, model, hyperparameters, n_eval, summary_path
@@ -45,13 +45,13 @@ def starting_train(
         # Loop over each batch in the dataset
         for i, batch in enumerate(train_loader):
             print(f"\rIteration {i + 1} of {len(train_loader)} ...", end="")
+            images, labels = batch
 
             optimizer.zero_grad()
-            predictions = model(batch.inputs)
-            loss = loss_fn(predictions, batch.labels)
+            predictions = model(images)
+            loss = loss_fn(predictions, labels)
             loss.backward()
             optimizer.step()
-            
 
             # Periodically evaluate our model + log to Tensorboard
             if step % n_eval == 0: